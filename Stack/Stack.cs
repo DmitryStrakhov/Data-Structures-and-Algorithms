f2206dbacb6ab60cb2bcd61b3a74f8d24666a86e
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2016-04-19 23:51:12 +0300

    #Stacks

--- Stack/Stack.cs
+++ Stack/Stack.cs
@@ -5,11 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Data_Structures_and_Algorithms {
-    public class Stack {
+    public class Stack<T> {
+        int size;
+        SinglyLinkedListNode<T> head;
 
+        public Stack() {
+            this.size = 0;
+            this.head = null;
+        }
+        public void Push(T value) {
+            this.head = new SinglyLinkedListNode<T>(value, this.head);
+            size++;
+        }
+        public T Pop() {
+            if(IsEmpty) {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            T value = this.head.Value;
+            this.head = this.head.Next;
+            this.size--;
+            return value;
+        }
+        public T Peek() {
+            if(IsEmpty) {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return this.head.Value;
+        }
+        public int Size {
+            get { return size; }
+        }
+        public bool IsEmpty {
+            get { return Size == 0; }
+        }
+        public bool IsFull {
+            get { return false; }
+        }
 
-
-
-
+        internal SinglyLinkedListNode<T> GetHead() {
+            return this.head;
+        }
     }
 }
--- Tests/Stack.cs
+++ Tests/Stack.cs
@@ -10,12 +10,70 @@ using NUnit.Framework;
 namespace Data_Structures_and_Algorithms.Tests {
     [TestFixture]
     public class StackTests {
+        [Test]
+        public void StackSimpleTest() {
+            Stack<int> stack = new Stack<int>();
+            Assert.IsNull(stack.GetHead());
+            Assert.IsTrue(stack.IsEmpty);
+        }
+        [Test]
+        public void StackPushTest() {
+            Stack<int> stack = new Stack<int>();
+            Assert.IsTrue(stack.IsEmpty);
+            Assert.AreEqual(0, stack.Size);
+            stack.Push(1);
+            Assert.IsFalse(stack.IsEmpty);
+            Assert.AreEqual(1, stack.Size);
+            stack.Push(1);
+            Assert.IsFalse(stack.IsEmpty);
+            Assert.AreEqual(2, stack.Size);
+        }
+        [Test]
+        public void StackIsFullTest() {
+            Stack<int> stack = new Stack<int>();
+            Assert.IsFalse(stack.IsFull);
+        }
 
+        [Test, ExpectedException(typeof(InvalidOperationException))]
+        public void StackPopGuardTest() {
+            Stack<int> stack = new Stack<int>();
+            int value = stack.Pop();
+        }
+        [Test]
+        public void StackPopTest() {
+            Stack<int> stack = new Stack<int>();
+            Assert.IsTrue(stack.IsEmpty);
+            Assert.AreEqual(0, stack.Size);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.IsFalse(stack.IsEmpty);
+            Assert.AreEqual(3, stack.Size);
+            Assert.AreEqual(3, stack.Pop());
+            Assert.IsFalse(stack.IsEmpty);
+            Assert.AreEqual(2, stack.Size);
+            Assert.AreEqual(2, stack.Pop());
+            Assert.IsFalse(stack.IsEmpty);
+            Assert.AreEqual(1, stack.Size);
+            Assert.AreEqual(1, stack.Pop());
+            Assert.IsTrue(stack.IsEmpty);
+            Assert.AreEqual(0, stack.Size);
+            Assert.IsNull(stack.GetHead());
+        }
 
-
-
-
-
+        [Test, ExpectedException(typeof(InvalidOperationException))]
+        public void StackPeekGuardTest() {
+            Stack<int> stack = new Stack<int>();
+            int value = stack.Peek();
+        }
+        [Test]
+        public void StackPeekTest() {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(11);
+            Assert.AreEqual(11, stack.Peek());
+            stack.Push(12);
+            Assert.AreEqual(12, stack.Peek());
+        }
     }
 }
 
