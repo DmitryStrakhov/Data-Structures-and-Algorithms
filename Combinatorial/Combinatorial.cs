002a9c31598138e79ea6bd79e27568b5f368a90f
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2021-05-30 00:22:40 +0300

    #Combinatorial.Combinations

---
+++ Combinatorial/Combinatorial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures_and_Algorithms {
+    public static class Combinatorial {
+        public static void Combinations<T>(IList<T> list, int k, Action<T[]> action) {
+            if(list == null) throw new ArgumentNullException(nameof(list));
+            if(action == null) throw new ArgumentNullException(nameof(action));
+            Guard.IsInRange(k, 1, list.Count, nameof(k));
+
+            T[] combination = new T[k];
+            CombinationsCore(list, combination, 0, list.Count - 1, 0, action);
+        }
+        public static void Combinations2<T>(IList<T> list, int k, Action<T[]> action) {
+            if(list == null) throw new ArgumentNullException(nameof(list));
+            if(action == null) throw new ArgumentNullException(nameof(action));
+            Guard.IsInRange(k, 1, list.Count, nameof(k));
+
+            T[] combination = new T[k];
+            int[] indices = new int[k];
+
+            for(int n = 0; n < k; n++) {
+                indices[n] = n;
+                combination[n] = list[n];
+            }
+
+            int listSize = list.Count;
+            while (indices[k - 1] < listSize) {
+                action(combination);
+
+                int t = k - 1;
+                while (t != 0 && indices[t] == listSize - k + t) {
+                    t--;
+                }
+                indices[t]++;
+
+                if(indices[t] < listSize) {
+                    combination[t] = list[indices[t]];
+                }
+
+                for (int i = t + 1; i < k; i++) {
+                    indices[i] = indices[i - 1] + 1;
+                    if(indices[i] < listSize) {
+                        combination[i] = list[indices[i]];
+                    }
+                }
+            }
+        }
+
+        static void CombinationsCore<T>(IList<T> list, T[] combination, int start, int end, int index, Action<T[]> action) {
+            if(index == combination.Length) {
+                action(combination);
+            }
+            else {
+                int max = Math.Min(end, end + 1 - combination.Length + index);
+                for (int n = start; n <= max; n++) {
+                    combination[index] = list[n];
+                    CombinationsCore(list, combination, n + 1, end, index + 1, action);
+                }
+            }
+        }
+    }
+}
---
+++ Tests/CombinatorialTests.cs
@@ -0,0 +1,92 @@
+﻿#if DEBUG
+
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data_Structures_and_Algorithms.Tests {
+    public abstract class CombinationsTestsBase {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void CombinationsGuardCase1Test() {
+            Combinations(null, 1, x => {});
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void CombinationsGuardCase2Test() {
+            Combinations(new int[1], 1, null);
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CombinationsGuardCase3Test() {
+            Combinations(new int[3], -1, x => {});
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CombinationsGuardCase4Test() {
+            Combinations(new int[3], 0, x => {});
+        }
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CombinationsGuardCase5Test() {
+            Combinations(new int[3], 4, x => {});
+        }
+        [TestMethod]
+        public void CombinationsTest1() {
+            int[] input = {1, 2, 3};
+
+            List<int> resultList = new List<int>(3);
+            Combinations(input, 1, x => {
+                Assert.AreEqual(1, x.Length);
+                resultList.Add(x[0]);
+            });
+            CollectionAssert.AreEqual(new[]{1, 2, 3}, resultList);
+        }
+        [TestMethod]
+        public void CombinationsTest2() {
+            int[] input = {1, 2, 3};
+
+            List<int> resultList = new List<int>(3);
+            Combinations(input, 3, x => {
+                Assert.AreEqual(3, x.Length);
+                resultList.AddRange(x);
+            });
+            CollectionAssert.AreEqual(new[]{1, 2, 3}, resultList);
+        }
+        [TestMethod]
+        public void CombinationsTest3() {
+            int[] input = { 0, 1, 2, 3, 4 };
+
+            List<int> resultList = new List<int>(64);
+            Combinations(input, 2, x => {
+                Assert.AreEqual(2, x.Length);
+                resultList.AddRange(x);
+            });
+            int[] expected = {
+                0, 1,
+                0, 2,
+                0, 3,
+                0, 4,
+                1, 2,
+                1, 3,
+                1, 4,
+                2, 3,
+                2, 4,
+                3, 4,
+            };
+            CollectionAssert.AreEqual(expected, resultList);
+        }
+
+        protected abstract void Combinations(IList<int> list, int k, Action<int[]> action);
+    }
+
+    [TestClass]
+    public class CombinationsTests : CombinationsTestsBase {
+        protected override void Combinations(IList<int> list, int k, Action<int[]> action) {
+            Combinatorial.Combinations(list, k, action);
+        }
+    }
+
+    [TestClass]
+    public class Combinations2Tests : CombinationsTestsBase {
+        protected override void Combinations(IList<int> list, int k, Action<int[]> action) {
+            Combinatorial.Combinations2(list, k, action);
+        }
+    }
+}
+#endif
