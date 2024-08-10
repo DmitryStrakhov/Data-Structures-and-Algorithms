a00f4b10f65bdeb8582783b9139293588250a056
Dmitry Strakhov <dmitry.strakhov.a@gmail.com>
2024-08-11 01:06:55 +0300

    #Randomness: Fisher-Yates algorithm

---
+++ Randomness.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data_Structures_and_Algorithms {
+    public static class Randomness {
+        public static void Shuffle<T>(IList<T> list) {
+            if(list == null) throw new ArgumentNullException(nameof(list));
+
+            Random random = new Random();
+
+            for(int i = list.Count - 1; i >= 1; i--) {
+                int j = random.Next(0, i + 1);
+                list.Swap(i, j);
+            }
+        }
+    }
+}
---
+++ Tests/Randomness.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Data_Structures_and_Algorithms.Tests {
+    [TestClass]
+    public class RandomnessTests {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))]
+        public void ShuffleGuardTest() {
+            Randomness.Shuffle((List<object>) null);
+        }
+        [TestMethod]
+        public void ShuffleSimpleTest() {
+            int[] array = {1, 2, 3, 4, 5, 6};
+            Randomness.Shuffle(array);
+        }
+    }
+}
